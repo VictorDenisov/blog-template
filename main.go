a95c62a525f101c78960f91fb1744530846f4f06
Victor Denisov <denisovenator@gmail.com>
2019-12-24 13:02:11 -0800

    Add cover image support to index page

--- main.go
+++ main.go
@@ -57,17 +57,29 @@ func navigationFunc() string {
 	return ""
 }
 
+func img_urlFunc(url string, size string) string {
+	return url
+}
+
 func main() {
+	type Site struct {
+		Url         string
+		Title       string
+		Description string
+		CoverImage  string
+	}
 	type Inventory struct {
 		Lang        string
 		MetaTitle   string
 		BlogUrl     string
 		CurrentPath string
+		Site        Site
 	}
 	funcMap := map[string]interface{}{
 		"asset":      assetFunc,
 		"body_class": body_classFunc,
 		"navigation": navigationFunc,
+		"img_url":    img_urlFunc,
 	}
 	header, err := template.New("header.tpl.html").Funcs(funcMap).ParseFiles("header.tpl.html")
 	if err != nil {
@@ -83,6 +95,12 @@ func main() {
 		MetaTitle:   "meta title",
 		BlogUrl:     "http://localhost",
 		CurrentPath: "/",
+		Site: Site{
+			Url:         "file:///home/vdenisov/projects/blogTemplate/index.html",
+			Title:       "Victor's blog",
+			Description: "This is Victor's blog",
+			CoverImage:  "https://static.ghost.org/v2.0.0/images/welcome-to-ghost.jpg",
+		},
 	}
 	tmpl, err := template.New("index.tpl.html").Funcs(funcMap).ParseFiles("index.tpl.html")
 
